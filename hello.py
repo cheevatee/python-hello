b7bcd25debbf86a9352b9d0970fc1c764f7a9def
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 00:06:08 +0700

    Update metrics.register_endpoint

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-metrics.register_endpoint('/metrics')
+metrics.register_endpoint('/app-metrics')
 
 #print(__name__)
 
