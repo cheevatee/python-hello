4549b8ee06ac674e62ed481c7808fdfc1821ac16
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 00:33:15 +0700

    Test

--- hello.py
+++ hello.py
@@ -1,30 +1,9 @@
-from flask import Flask, render_template, jsonify
-##from prometheus_flask_exporter import PrometheusMetrics
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
-from flask_prometheus_metrics import register_metrics
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-##metrics = PrometheusMetrics(app)
-
-#print(__name__)
-
-# static information as metric
-##metrics.info('app_info', 'Application info', version='1.0.3')
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
-def home():
-#    return 'Hello World!'
-#    return render_template('home-hello.html')
-    return render_template('home-hello.html', name='Tee')
-
-@app.route('/about')
-def about():
-    return 'This is a about page'
-
-register_metrics(app, app_version="v0.1.2", app_config="staging")
-dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
-
-if __name__ == '__main__':  # Script executed directly?
-#    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
-    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
+def main():
+    return 'OK'
