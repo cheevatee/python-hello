44ddc43298811e460e63138daf39ce404e411b22
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 00:40:00 +0700

    Add PrometheusMetrics

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+#print(__name__)
+
 @app.route('/')
-def main():
-    return 'OK'
+def home():
+#    return 'Hello Flask!'
+#    return render_template('home-hello.html')
+    return render_template('home-hello.html', name='Tee')
+
+@app.route('/about')
+def about():
+    return 'This is a about page'
