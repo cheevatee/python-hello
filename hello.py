5fbd55ba9fa474655f39e2696fe5284b47bd5a51
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 00:45:39 +0700

    Update app.run

--- hello.py
+++ hello.py
@@ -15,3 +15,7 @@ def home():
 @app.route('/about')
 def about():
     return 'This is a about page'
+
+if __name__ == '__main__':  # Script executed directly?
+#    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
+     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
