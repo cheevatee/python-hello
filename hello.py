26fb75e2d7ab757d05e64925d6155fb15e9ac6c9
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 01:26:55 +0700

    Update hello.py 1

--- hello.py
+++ hello.py
@@ -17,26 +17,6 @@ def home():
 @app.route('/about')
 def about():
     return 'This is a about page'
-(url-shortener) [root@bastion url-shortener]# cat hello.py
-from flask import Flask, render_template
-from prometheus_flask_exporter import PrometheusMetrics
-
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
-metrics.info('app_info', 'Application info', version='1.0.3')
-
-#print(__name__)
-
-@app.route('/')
-def home():
-#    return 'Hello Flask!'
-#    return render_template('home-hello.html')
-    return render_template('home-hello.html', name='Tee')
-
-@app.route('/about')
-def about():
-    return 'This is a about page'
 
 @app.route('/test')
 def about():
