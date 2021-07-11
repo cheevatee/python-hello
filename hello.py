f9b302bd60b133fc83f456a4a29918a53d5c825d
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 01:23:01 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -13,12 +13,34 @@ def home():
 #    return 'Hello Flask!'
 #    return render_template('home-hello.html')
     return render_template('home-hello.html', name='Tee')
-    pass
+
+@app.route('/about')
+def about():
+    return 'This is a about page'
+(url-shortener) [root@bastion url-shortener]# cat hello.py
+from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+#print(__name__)
+
+@app.route('/')
+def home():
+#    return 'Hello Flask!'
+#    return render_template('home-hello.html')
+    return render_template('home-hello.html', name='Tee')
+
+@app.route('/about')
+def about():
+    return 'This is a about page'
 
 @app.route('/test')
 def about():
     return 'Hello test'
-    pass
 
 if __name__ == '__main__':  # Script executed directly?
 #    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
