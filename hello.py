81724d32405a392140daddfa7336629b2e5ea26b
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 22:13:01 +0700

    Test

--- hello.py
+++ hello.py
@@ -1,35 +1,45 @@
 import time
 import random
 
-from flask import Flask, render_template
+from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
 PrometheusMetrics(app)
 
+
 endpoints = ('one', 'two', 'three', 'four', 'five', 'error')
 
-@app.route('/')
-def home():
-    return render_template('home-hello.html', name='Tee')
 
-@app.route('/ping')
-def pingpong():
-    return 'pong'
-
-@app.route('/fast')
-def fast():
+@app.route('/one')
+def first_route():
     time.sleep(random.random() * 0.2)
     return 'ok'
 
-@app.route('/slow')
-def slow():
-    time.sleep(6)
+
+@app.route('/two')
+def the_second():
+    time.sleep(random.random() * 0.4)
     return 'ok'
+
+
+@app.route('/three')
+def test_3rd():
+    time.sleep(random.random() * 0.6)
+    return 'ok'
+
+
+@app.route('/four')
+def fourth_one():
+    time.sleep(random.random() * 0.8)
+    return 'ok'
+
 
 @app.route('/error')
 def oops():
     return ':(', 500
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, threaded=True)
+    app.run('0.0.0.0', 5000, threaded=True)
