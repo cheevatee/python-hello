0ed0bc25b4c182b19e8f5139b7d2d43fc901e151
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 16:23:46 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -29,4 +29,5 @@ def oops():
     return ':(', 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True,use_reloader=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True, use_reloader=True)
+
