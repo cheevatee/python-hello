3e923767439e59029367376b2378899540d058e3
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 13:43:14 +0700

    Add run_simple

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
 from flask_prometheus_metrics import register_metrics
+
 
 app = Flask(__name__)
 
@@ -24,4 +27,5 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {'/metrics': make_wsgi_app()})
 
 if __name__ == '__main__':  # Script executed directly?
 #    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
-     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
+#    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
+    run_simple("0.0.0.0", 8080, create_dispatcher(), use_reloader=True, use_debugger=True, use_evalex=True,)
