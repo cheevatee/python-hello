2684d1352cb81aff88b40042efd84e98166b24cc
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 16:35:43 +0700

    Fix all issues

--- hello.py
+++ hello.py
@@ -4,10 +4,8 @@ import random
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
-
 app = Flask(__name__)
 PrometheusMetrics(app)
-
 
 endpoints = ('one', 'two', 'three', 'four', 'five', 'error')
 
@@ -19,9 +17,14 @@ def home():
 def about():
     return 'pong'
 
-@app.route('/one')
+@app.route('/fast')
 def first_route():
     time.sleep(random.random() * 0.2)
+    return 'ok'
+
+@app.route('/slow')
+def first_route():
+    time.sleep(2)
     return 'ok'
 
 @app.route('/error')
@@ -30,5 +33,3 @@ def oops():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True)
-#, debug=True,use_reloader=True
-
