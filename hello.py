090f15e7546c88c3edacf9cc7ae2dd7b175838ae
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 15:33:24 +0700

    Test 1

--- hello.py
+++ hello.py
@@ -1,45 +1,20 @@
-import time
-import random
-
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
-
 
 app = Flask(__name__)
 PrometheusMetrics(app)
 
+#print(__name__)
 
-endpoints = ('one', 'two', 'three', 'four', 'five', 'error')
+endpoints = ('test', 'ping')
 
+@app.route('/test')
+def home():
+    return render_template('home-hello.html', name='Test')
 
-@app.route('/one')
-def first_route():
-    time.sleep(random.random() * 0.2)
-    return 'ok'
+@app.route('/ping')
+def about():
+    return 'pong'
 
-
-@app.route('/two')
-def the_second():
-    time.sleep(random.random() * 0.4)
-    return 'ok'
-
-
-@app.route('/three')
-def test_3rd():
-    time.sleep(random.random() * 0.6)
-    return 'ok'
-
-
-@app.route('/four')
-def fourth_one():
-    time.sleep(random.random() * 0.8)
-    return 'ok'
-
-
-@app.route('/error')
-def oops():
-    return ':(', 500
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, threaded=True)
+if __name__ == '__main__':  # Script executed directly?
+     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
