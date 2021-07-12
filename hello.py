669111edeb215621248b8ad46ccef76f346b24ab
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-07-12 16:06:29 +0700

    Test 5

--- hello.py
+++ hello.py
@@ -11,35 +11,22 @@ PrometheusMetrics(app)
 
 endpoints = ('one', 'two', 'three', 'four', 'five', 'error')
 
+@app.route('/test')
+def home():
+    return render_template('home-hello.html', name='Test')
+
+@app.route('/ping')
+def about():
+    return 'pong'
 
 @app.route('/one')
 def first_route():
     time.sleep(random.random() * 0.2)
     return 'ok'
 
-
-@app.route('/two')
-def the_second():
-    time.sleep(random.random() * 0.4)
-    return 'ok'
-
-
-@app.route('/three')
-def test_3rd():
-    time.sleep(random.random() * 0.6)
-    return 'ok'
-
-
-@app.route('/four')
-def fourth_one():
-    time.sleep(random.random() * 0.8)
-    return 'ok'
-
-
 @app.route('/error')
 def oops():
     return ':(', 500
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, threaded=True)
